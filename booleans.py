c4effba4609206f6d3eab5ba01f6dcdce481c01e
Kazi Kaiser <153204277+KaiserKazi@users.noreply.github.com>
2024-04-21 19:15:07 +0600

    Add files via upload

---
+++ booleans.py
@@ -0,0 +1,15 @@
+print (10 > 9)
+print (10 == 9)
+print (10 < 9)
+
+a = 100
+b = 101
+
+if b > a:
+    print ("b is greater than a")
+else:
+    print ("b is not greater than a")
+    
+print (bool("Hello"))
+print (bool(15))
+print (bool([]))
